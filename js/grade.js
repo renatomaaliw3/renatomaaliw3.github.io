ba45dc9cb07ffb0587c1200b36230b1aa01ee620
renatomaaliw3 <renatomaaliw3@yahoo.com>
2023-11-03 20:44:26 +0800

    Save

--- js/grade.js
+++ js/grade.js
@@ -1,89 +1,29 @@
-/* global variables */
+/* glob
+al variables */
 
 let typingTimer;
 const doneTypingInterval = 1500; // 1 second delay
+ 
+ // You can replace the URL with your external JSON file's location
 
  $(document).ready(function() {
 
         function performSearch() {
 
-            //clearDisplay();
-
             const searchTerm = $('#emailInput').val();
 
-            // You can replace the URL with your external JSON file's location
-            const jsonURL = 'checkpoint/csvjson.json';
+            const jsonScores = 'checkpoint/csvjson.json';
 
-            $.getJSON(jsonURL, function(data) {
+            $.getJSON(jsonScores, function(data) {
 
-                const exactMatch = data.find(item => item['Email'] == searchTerm);
+                var exactMatch = data.find(item => item['Email'] == searchTerm);
 
                 if (exactMatch) {
 
-                    $('#searchResult').html(
-
-                        '<h2 class="text-primary">' + exactMatch['First Name'] + ' ' + exactMatch['Last Name'] + "'s " + ' Grade' + '</h2>' +
-
-                        '<table class="table table-bordered">' + 
-
-                            '<thead class="bg-dark text-white">' +
-
-                                '<th colspan = 14 class="h4">' + exactMatch['Term'] + '</th>' + 
-
-                            '</thead>' +
-
-                            '<thead>' +
-
-                                '<tr class="bg-secondary">' + 
-                             
-                                    '<th>Lecture<br>Quiz (30%)</th>' +
-                                    '<th>Lecture<br>Quiz Points</th>' +
-                                    '<th>Lecture<br>Major Exam (40%)</th>' +
-                                    '<th>Lecture<br>Major Exam Points</th>' +
-                                    '<th>Lecture<br>Performance (30%)</th>' +
-                                    '<th>Lecture<br>Performance Points</th>' +
-                                    '<th>Lab Activities (40%)</th>' +
-                                    '<th>Lab Activities Points</th>' +
-                                    '<th>Lab Major Exam (60%)</th>' +
-                                    '<th>Lab Major Exam Points</th>' +
-                                    '<th>Lecture Term Grade (60%)</th>' +
-                                    '<th>Lab Term Grade (40%)</th>' +
-                                    '<th>Overall Term Grade</th>' +
-                                    '<th>Equivalent </th>' +
-
-                                '</tr>' +
-
-                            '</thead>' +
-
-                            '<tbody>' +
-
-                                '<tr>' + 
-                                 
-                                    '<td>' + exactMatch['Lecture Quiz (30%)'] + '</td>' +
-                                    '<td>' + exactMatch['Lecture Quiz Points'] + '</td>' +
-                                    '<td>' + exactMatch['Lecture Major Exam (40%)'] + '</td>' +
-                                    '<td>' + exactMatch['Lecture Major Exam Points'] + '</td>' +
-                                    '<td>' + exactMatch['Lecture Performance (30%)'] + '</td>' +
-                                    '<td>' + exactMatch['Lecture Performance Points'] + '</td>' +
-                                    '<td>' + exactMatch['Lab Activities (40%)'] + '</td>' +
-                                    '<td>' + exactMatch['Lab Activities Points'] + '</td>' +
-                                    '<td>' + exactMatch['Lab Major Exam (60%)'] + '</td>' +
-                                    '<td>' + exactMatch['Lab Major Exam Points'] + '</td>' +
-                                    '<td>' + exactMatch['Lecture Term Grade (60%)'] + '</td>' +
-                                    '<td>' + exactMatch['Lab Term Grade (40%)'] + '</td>' +
-                                    '<td>' + exactMatch['Overall Term Grade'] + '</td>' +
-                                    '<td class="bg-success">' + exactMatch['Equivalent'] + '</td>' +
-
-                                '</tr>' +
-
-                            '</tbody>' +
-
-                        '</table>'
-                    );
+                    constructTable(exactMatch);
 
                 } else {
 
-                    
                     $('#searchResult').html('<h3 class="text-danger text-md">Email not found</h3>');
                 }
 
@@ -91,13 +31,73 @@ const doneTypingInterval = 1500; // 1 second delay
 
         }
 
-        function clearDisplay() {
+        function constructTable(exactMatch) {
 
-            $('#searchResult').empty();
+            var contents =  '<h1 class="text-primary font-weight-bold">' + exactMatch['First Name'] + ' ' + exactMatch['Last Name'] + '</h1>';
+            contents +=     '<table class="table table-bordered table-responsive">' + 
+
+                                '<thead class="bg-dark text-white">' +
+
+                                    // '<th colspan = 14 class="h4">' + exactMatch['Term'] + '</th>' + 
+
+                                '</thead>';
+
+            contents +=         '<thead>'
+            contents +=             '<tr id="headerLabels" class="bg-secondary">';
+
+                    for (var key in exactMatch) {
+
+                        if (exactMatch.hasOwnProperty(key)) {
+
+                            if (key == 'Email' || key == 'Course' || key == 'Section' || key == 'Last Name' || key == 'First Name' || key == 'No.') {
+                                
+                                continue;
+
+                            } else {
+
+                                contents += '<th>' + key + '</th>';
+
+                            }
+
+                        }
+
+                    }
+
+            contents +=             '</tr>';
+            contents +=         '</thead>';
+
+            contents +=         '<tbody>';
+            contents +=             '<tr id="scoreData">';
+
+                    for (var key in exactMatch) {
+
+                        if (exactMatch.hasOwnProperty(key)) {
+
+                            if (key == 'Email' || key == 'Course' || key == 'Section' || key == 'Last Name' || key == 'First Name' || key == 'No.') {
+                                
+                                continue;
+
+                            } else {
+
+                                var value = exactMatch[key]
+
+                                contents += '<td>' + value + '</td>';
+
+                            } 
+
+                        }
+
+                    }
+
+            contents +=             '</tr>';
+            contents +=         '</tbody>';
+
+            $('#searchResult').html(contents);
 
         }
 
-         $('#emailInput').on('keyup', function() {
+
+        $('#emailInput').on('keyup', function() {
 
                 clearTimeout(typingTimer);
                 typingTimer = setTimeout(performSearch, doneTypingInterval);
