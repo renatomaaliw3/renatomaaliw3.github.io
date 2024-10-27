3d58fab900973997d0809f9a2ccadf493eeabedf
renatomaaliw3 <renatomaaliw3@yahoo.com>
2024-10-27 19:24:39 +0800

    Save

--- js/grade.js
+++ js/grade.js
@@ -6,6 +6,9 @@ const doneTypingInterval = 1350; // 1 second delay
  
  $(document).ready(function() {
 
+        const gradeTable = $('#gradeTable');
+        const searchResult = $('#searchResult')
+
         const searchTerm = $('#emailInput').val();
         const dropDown = $('#courseMenu').val();
         var direct = 'checkpoint/';
@@ -48,97 +51,91 @@ const doneTypingInterval = 1350; // 1 second delay
 
         function constructTable(data, exactMatch, searchTerm) {
 
-            var contents =  '<h1 class="text-primary font-weight-bold">' + exactMatch['First Name'] + ' ' + exactMatch['Last Name'] + '</h1>';
-            contents +=     '<table class="table table-bordered table-responsive" id="gradeTable">';
-
+            var contents =  '<h3 class="text-primary text-left font-weight-bold">' + exactMatch['First Name'] + ' ' + exactMatch['Last Name'] + '</h3>';
+            contents +=     '<table class="table table-bordered table-responsive" id="gradeTable" style="display: table;">';
+        
             contents +=         '<thead>';
             contents +=             '<tr id="headerLabels" class="bg-secondary">';
+        
+            var keysToShow = ['Term', 'Lecture Quiz', 'Lecture Quiz Points (max = 30%)',
+                             'Lecture Major Exam', 'Lecture Major Exam Points (max = 40%)',
+                             'Lecture Performance', 'Lecture Performance Points (max = 30%)',
+                             'Lab Activities', 'Lab Activities Points (max = 40%)',
+                             'Lab Major Exam','Lab Major Exam Points (max = 60%)',
+                             'Lecture Term Grade (60%)', 
+                             'Lab Term Grade (40%)', 'Lecture Term Grade (E)', 'Lab Term Grade (E)'];
+        
+            keysToShow.forEach(function(key) {
 
-                    for (var key in exactMatch) { // get JSON keys
+                if (key == 'Lecture Term Grade (E)' || key == 'Lab Term Grade (E)' || key == 'Term') {
 
-                        //console.log(key)
+                    contents += '<th class="' + clean_key(key) + '">' + key + '</th>'; // display specified keys as headers
 
-                        if (exactMatch.hasOwnProperty(key)) { 
+                } else {
 
-                            if (key == 'Email' || key == 'Course' || key == 'Section' || key == 'Last Name' || key == 'First Name' || key == 'No.') {
-                                
-                                continue; //skip this keys as headers
+                    contents += '<th class="' + clean_key(key) + '"' + ' style="display: none;">' + key + '</th>'; // display specified keys as headers
+
+                }
+
+            });
+        
+            contents +=             '</tr>';
+            contents +=         '</thead>';
+            contents +=         '<tbody>';
+                
+            data.forEach(function(item) {
+
+                var email = item['Email'].toLowerCase();
+        
+                if (email == searchTerm) {
+
+                    contents += '<tr id="scoreData">';
+        
+                    keysToShow.forEach(function(key) {
+
+                        if (key === 'Lecture Term Grade (E)' || key === 'Lab Term Grade (E)' || key === 'Term') {
+
+                            if (item[key] == 4.00) {
+
+                                contents += '<td class="text-warning">' + decimal_places(item[key]) + '</td>';
+
+                            } else if (item[key] == 5.00) {
+
+                                contents += '<td class="text-danger">' + decimal_places(item[key]) + '</td>';
 
                             } else {
 
-                                contents += '<th>' + key + '</th>'; // if not skipped (keys), display it as headers
+                                contents += '<td class="' + clean_key(key) + '">' + decimal_places(item[key]) + '</td>';
 
                             }
+
+                        } else {
+
+                            contents += '<td class="' + clean_key(key) + '"' + ' style="display: none;">' + decimal_places(item[key]) + '</td>';
 
                         }
 
-                    }
-
-            contents +=             '</tr>';
-            contents +=         '</thead>';
-
-            contents +=         '<tbody>';
-                 
-                                    
-                        data.forEach(function(item) {
-
-                            var email = item['Email'].toLowerCase();
-
-                            if (email == searchTerm) {
-
-                                contents += '<tr id="scoreData">';
-
-                                for (var key in exactMatch) {
-
-                                     if (key == 'Email' || key == 'Course' || key == 'Section' || key == 'Last Name' || key == 'First Name' || key == 'No.') {
-                                
-                                        continue; // skip this values
-
-                                    } else {
-
-                                         // Check if the key is 'Lecture Term Grade (E)' or 'Lab Term Grade (E)'
-                                        if ((key === 'Lecture Term Grade (E)' || key === 'Lab Term Grade (E)') && (item[key] == 4.00)) {
-
-                                            // Apply warning color if the value is 4.00
-                                            contents += '<td class="text-warning">' + decimal_places(item[key]) + '</td>';
-
-                                        } else if ((key === 'Lecture Term Grade (E)' || key === 'Lab Term Grade (E)') && (item[key] == 5.00)) {
-
-                                             // Apply danger color if the value is 4.00
-                                            contents += '<td class="text-danger">' + decimal_places(item[key]) + '</td>';
-
-                                        } else {
-
-                                            // Default color for other values
-                                            contents += '<td>' + decimal_places(item[key]) + '</td>';
-
-                                        }
-
-                                    }
-
-                                }
-
-                                contents += '</tr>';
-
-                            }
-
-                        });
-
+                    });
+        
+                    contents += '</tr>';
+                }
+            });
+        
             contents +=         '</tbody>';
-            contents +=         '</table>';
-
-            contents +=         '<table id="gradeLegend">';
-
-            contents +=             '<tr><td colspan=2> Equivalent Rating </td></tr><tr><td> 1.00 </td><td> 98 - 100 </td></tr><tr><td> 1.25 </td><td> 95 - 97 </td></tr><tr><td> 1.50 </td><td> 92 - 94 </td></tr><tr><td> 1.75 </td><td> 89 - 91 </td></tr><tr><td> 2.00 </td><td> 86 - 88 </td></tr><tr><td> 2.25 </td><td> 83 - 85 </td></tr><tr><td> 2.50 </td><td> 80 - 82 </td></tr><tr><td> 2.75 </td><td> 77 - 79 </td></tr><tr><td> 3.00 </td><td> 75 - 79 </td></tr><tr><td class=text-warning> 4.00 </td><td class=text-warning> 70 - 74 </td></tr><tr><td class=text-danger> 5.00 </td><td class=text-danger> 50 - 69 </td></tr>';                         
-                     
-            contents +=         '</table>';
-            contents +=         '<h6 class="info mt-5" style="font-size: 0.75rem;"> As per SLSU University Code, Chapter 55 (Honors, Art. 443 - Computation of Grades), the rounding off of final grades shall not be allowed. </h6>';
-
+            contents +=     '</table>';
+        
+            // Button for details
+            contents += '<div style="display: flex;"><input type="button" value="Show Details" id="btnDetails"></div>';
+        
+            contents += '<table id="gradeLegend" style="display: none;">';
+            contents += '<tr><td colspan=2> Equivalent Rating </td></tr><tr><td> 1.00 </td><td> 98 - 100 </td></tr><tr><td> 1.25 </td><td> 95 - 97 </td></tr><tr><td> 1.50 </td><td> 92 - 94 </td></tr><tr><td> 1.75 </td><td> 89 - 91 </td></tr><tr><td> 2.00 </td><td> 86 - 88 </td></tr><tr><td> 2.25 </td><td> 83 - 85 </td></tr><tr><td> 2.50 </td><td> 80 - 82 </td></tr><tr><td> 2.75 </td><td> 77 - 79 </td></tr><tr><td> 3.00 </td><td> 75 - 79 </td></tr><tr><td class=text-warning> 4.00 </td><td class=text-warning> 70 - 74 </td></tr><tr><td class=text-danger> 5.00 </td><td class=text-danger> 50 - 69 </td></tr>';                         
+            contents += '</table>';
+            contents += '<h6 class="info mt-5" style="font-size: 0.75rem;"> As per SLSU University Code, Chapter 55 (Honors, Art. 443 - Computation of Grades), the rounding off of final grades shall not be allowed. </h6>';
+        
             $('#searchResult').html(contents);
 
         }
-
-
+        
         $('#emailInput').on('keyup', function() {
 
                 clearTimeout(typingTimer);
@@ -152,6 +149,30 @@ const doneTypingInterval = 1350; // 1 second delay
                 
         });
 
+        $('#searchResult').on('click', '#btnDetails', function() {
+
+            const button = $(this);
+
+            if (button.val() == 'Show Details') {
+
+                button.val('Hide Details')
+                searchResult.find('th, td').show(); //Show all
+                $(this).parents('#searchResult').find('#gradeTable').css({'display': 'block'});
+                $(this).parents('#searchResult').find('#gradeLegend').show()
+
+
+            } else {
+
+                button.val('Show Details');
+                searchResult.find('#gradeTable th, #gradeTable td').not('.Term, .LectureTermGradeE, .LabTermGradeE').hide(); //Hide class except
+                $(this).parents('#searchResult').find('#gradeTable').css({'display': 'table'});
+                $(this).parents('#searchResult').find('#gradeLegend').css({'display': 'none'});
+                
+            }
+
+        });
+
+        // Functions
         function emailNotFound() {
 
             $('#searchResult').html('<h3 class="text-danger text-md">Email not found</h3>');
@@ -172,5 +193,12 @@ const doneTypingInterval = 1350; // 1 second delay
 
         }
 
+        function clean_key(value) {
+
+            var the_key = value;
+
+            return the_key.replace(/[\s()=%/]+/g,''); //remove spaces and characters (special)
+
+        }
 
 });
