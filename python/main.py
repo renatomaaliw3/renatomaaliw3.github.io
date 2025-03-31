5cf3cf9274dfbf45b60a44a3ff2ea69c982b40b4
renatomaaliw3 <renatomaaliw3@yahoo.com>
2025-04-01 01:29:56 +0800

    Save

--- python/main.py
+++ python/main.py
@@ -1,36 +1,77 @@
-import matplotlib.pyplot as plt
+# Imports
+
 import pandas as pd
-import matplotlib.tri as tri
-import numpy as np
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+import io
 from pyscript import display
+from pyodide.http import open_url
 
-# First create the x and y coordinates of the points.
-n_angles = 36
-n_radii = 8
-min_radius = 0.25
-radii = np.linspace(min_radius, 0.95, n_radii)
+import warnings
+warnings.filterwarnings('ignore')
 
-angles = np.linspace(0, 2 * np.pi, n_angles, endpoint=False)
-angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
-angles[:, 1::2] += np.pi / n_angles
+from matplotlib.ticker import FormatStrFormatter
 
-x = (radii * np.cos(angles)).flatten()
-y = (radii * np.sin(angles)).flatten()
-z = (np.cos(radii) * np.cos(3 * angles)).flatten()
+url = "checkpoint/cpe21-24-25.json"  # Adjust the path if needed
+response = open_url(url)
+data_str = response.read()  # Already a string, no need to decode
+df = pd.read_json(io.StringIO(data_str))
 
-# Create the Triangulation; no triangles so Delaunay triangulation created.
-triang = tri.Triangulation(x, y)
+# Prelim Graphs
 
-# Mask off unwanted triangles.
-triang.set_mask(np.hypot(x[triang.triangles].mean(axis=1),
-                            y[triang.triangles].mean(axis=1))
-                < min_radius)
+course = 'CPE21'
+term = 'Preliminary'
+# email = my_email_value
 
-fig1, ax1 = plt.subplots()
-ax1.set_aspect('equal')
-tpc = ax1.tripcolor(triang, z, shading='flat')
-fig1.colorbar(tpc)
-ax1.set_title('tripcolor of Delaunay triangulation, flat shading')
+# your_grade = df.query("Email == @email and Term == @term")['Lecture Term Grade (60%)'].values.flatten()[0]
+mean_grade = df['Lecture Term Grade (60%)'].mean()
 
-display(fig1, target="mpl")
+# Prelims Histogram
+
+course = 'CPE21'
+term = 'Preliminary'
+email = 'anj@mail.com'
+your_grade = df.query("Email == @email and Term == @term")['Lecture Term Grade (60%)'].values.flatten()[0]
+mean_grade = df['Lecture Term Grade (60%)'].mean()
+
+fig, ax = plt.subplots(1,2, figsize = (15, 5))
+fig.suptitle('Preliminary Term Statistics', fontweight = 'bold', fontsize = 15)
+
+# 1st Graph
+ax[0].set_title(f'Histogram of Lecture Grades')
+sns.histplot(df['Lecture Term Grade (60%)'], kde = False, bins = 20, color = 'aqua', stat = 'density', ax = ax[0])
+sns.kdeplot(df['Lecture Term Grade (60%)'], color = 'black', linewidth = 3, alpha = 0.75, zorder = 10, ax = ax[0])
+ax[0].axvline(mean_grade, linestyle = '--', color = 'red', linewidth = 3, label = f'Average Lecture Grade is {mean_grade:.3f}', alpha = 0.75)
+# ax[0].axvline(your_grade, linestyle = '--', color = 'blue', linewidth = 2, label = f'Your Grade is {round(your_grade, 3)}', alpha = 0.75)
+ax[0].legend(loc = 'upper left')
+ax[0].set_xlabel(f'Lecture Term Grades')
+ax[0].grid(True, linestyle = '--', alpha = 0.5)
+
+# 2nd Graph
+
+ax[1].set_title(f'Count Plot of Lecture Grades (Equivalent)')
+ax[1] = sns.countplot(x = 'Lecture Term Grade (E)', data = df, color = 'orange', alpha = 0.85)
+# ax[1].legend(loc = 'upper left')
+# ax[1].set_xlabel(f'{term} Term Grades (E)')
+ax[1].grid(True, linestyle = '--', alpha = 0.5)
+
+
+# Annotate each bar with the count value
+for p in ax[1].patches:
+
+    height = p.get_height()
+    ax[1].text(p.get_x() + p.get_width() / 2, height + 0.08, f'{int(height)}', 
+            ha='center', va='bottom')
+    
+# Reformat the x-axis tick labels to show two decimals
+ax[1].set_xticklabels([f'{float(label.get_text()):.2f}' for label in ax[1].get_xticklabels()])
+
+# Calculate the maximum count from the count plot
+max_count = max(p.get_height() for p in ax[1].patches)
+
+# Set the x-axis limits on the second subplot to be from 0 to 0.5 times the maximum count
+ax[1].set_ylim(0, 4 + max_count)
+    
+plt.tight_layout()
+
+display(fig, target="grph")
